7feab59e1a46ba462eac5d2b16fe7704705e3bf2
TashviGulati <109961072+TashviGulati@users.noreply.github.com>
2022-08-29 16:35:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("hi i am console screen");
+
+var firstName="Tashvi ";
+var lastName="Gulati";
+var fullname=firstName.concat(lastName);
+console.log(fullname);
+
+var lengthOfName=fullname.length;
+console.log(lengthOfName);
+newName=fullname.replace("Tashvi","Tash");
+console.log(newName);
+
+console.log(newName.toLowerCase());
+console.log(newName.toUpperCase());
+
+console.log(Math.sqrt(100));
+console.log(Math.max(500,200));
+console.log(Math.min(300,150))
+console.log(Math.pow(4,3))
